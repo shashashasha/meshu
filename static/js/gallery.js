452c32ebc656acf4119101bd907c2a3b20d3425c
binx <rachelbinx@gmail.com>
2012-01-25 01:19:00 -0800

    making make and gallery links work

--- static/js/gallery.js
+++ static/js/gallery.js
@@ -5,7 +5,7 @@ $(function() {
 	for (var i = 1; i < 9; i++){
 		$("<li>")
 			.attr("class","object")
-			.append($("<div>").addClass("image").css("background-image","url(images/meshu_0"+i+".png)"))
+			.append($("<div>").addClass("image").css("background-image","url(../../static/images/meshu_0"+i+".png)"))
 			.append($("<div>").addClass("description").text(desc[Math.floor(Math.random()*6)]))
 			.append($("<div>").addClass("detail").text("Click for more information"))
 			.appendTo("ul.gallery");
