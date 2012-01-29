7472aecfb785983a3c08be9d112eee62fd7a15ed
binx <rachelbinx@gmail.com>
2012-01-28 17:47:26 -0800

    tiny things

--- static/js/app.js
+++ static/js/app.js
@@ -3,18 +3,11 @@ $(function() {
 	var place = window.location.hash;
 	if(place.charAt(0) == '#')
     	place = place.substr(1);
-	$("#content").addClass(place);
-	console.log(place);
+	$("#content").attr("class",place);
 
 	// create a meshu object for every frame class div
 	$(".frame").each(function(i, e) {
 		var meshu = sb.meshu(e);
-	});
-	$("#next").click(function(){
-	    $("#content").addClass("materials")
-	});
-	$("#back").click(function(){
-	    $("#content").removeClass("materials")
 	});
 	$(".option-list li").click(function(){
 		$(this).parent().find("li").removeClass("selected");
