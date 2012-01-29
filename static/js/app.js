5e38348bc2281e892fd44e2d0867cd1d9121e570
binx <rachelbinx@gmail.com>
2012-01-28 18:31:28 -0800

    back to dumb logic

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,13 @@
 $(function() {
-
-	var place = window.location.hash;
-	if(place.charAt(0) == '#')
-    	place = place.substr(1);
-	$("#content").attr("class",place);
-
 	// create a meshu object for every frame class div
 	$(".frame").each(function(i, e) {
 		var meshu = sb.meshu(e);
+	});
+	$("#next").click(function(){
+	    $("#content").attr("class","materials")
+	});
+	$("#back").click(function(){
+	    $("#content").attr("class","edit")
 	});
 	$(".option-list li").click(function(){
 		$(this).parent().find("li").removeClass("selected");
