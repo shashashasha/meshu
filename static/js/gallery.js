bf21c1ccbd3b042f5dd6eb235bee1c2e49569442
Sha Hwang <shashashasha@gmail.com>
2012-02-05 21:21:18 -0800

    css tweaks

--- static/js/gallery.js
+++ static/js/gallery.js
@@ -1,21 +1,22 @@
 $(function() {
 
-	var desc = ["Japan","European Countries","SE Asia","US Cities","Spain","China"];
+	// var desc = ["Japan","European Countries","SE Asia","US Cities","Spain","China"];
 
-	for (var i = 1; i < 9; i++){
-		$("<li>")
-			.attr("class","object")
-			.append($("<div>").addClass("image").css("background-image","url(../../static/images/meshu_0"+i+".png)"))
-			.append($("<p>").addClass("description").text(desc[Math.floor(Math.random()*6)]))
-			.append($("<a>").addClass("clickthrough").text("Click for more information"))
-			.appendTo("ul.gallery")
-			.click(function() {
-				var j = i;
-				return function() {
-					window.location = '/shop/' + j;
-				};
-			}());
-	}
+	// for (var i = 1; i < 9; i++){
+	// 	$("<li>")
+	// 		.attr("class","object")
+	// 		.append($("<div>").addClass("image").css("background-image","url(../../static/images/meshu_0"+i+".png)"))
+	// 		.append($("<p>").addClass("description").text(desc[Math.floor(Math.random()*6)]))
+	// 		.append($("<a>").addClass("clickthrough").text("Click for more information"))
+	// 		.appendTo("ul.gallery")
+	// 		.click(function() {
+	// 			var j = i;
+	// 			return function() {
+	// 				window.location = '/shop/' + j;
+	// 			};
+	// 		}());
+	// }
+
 	d3.selectAll("#master-svg").attr("class","meshu-svg");
 	d3.selectAll("#delaunay").attr("transform","scale(.36)");
 	
