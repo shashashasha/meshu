7a78567d5b10ac5c4c7f419cfcf6a66e348d36d6
Sha Hwang <shashashasha@gmail.com>
2012-02-19 15:09:34 -0800

    saving as new, saving as current

--- meshu/views.py
+++ meshu/views.py
@@ -99,7 +99,24 @@ def item_handler(request, item_id, template, view):
 			'view': view
 		}, context_instance = RequestContext(request))
 
-def item_save(request, item_encoded):
+def meshu_update(request, item_encoded):
+	xhr = request.GET.has_key('xhr')
+
+	item_id = int(str(item_encoded).decode("hex"))
+	old = Meshu.objects.get(id=item_id)
+
+	old = meshu_update(request, old)
+	old.save()
+
+	if xhr:
+		response_dict = {}
+		response_dict.update({ 'success' : True })
+		response_dict.update({ 'meshu_id' : old.id })
+		return HttpResponse(simplejson.dumps(response_dict), mimetype='application/javascript')
+
+	return item_handler(request, item_id, 'item.html', 'view')
+
+def meshu_save(request, item_encoded):
 	xhr = request.GET.has_key('xhr')
 
 	item_id = int(str(item_encoded).decode("hex"))
@@ -212,7 +229,7 @@ def mail_order_confirmation(email, meshu, order):
 #
 # Ordering!
 #
-def item_order(request, item_id):
+def order_meshu(request, item_id):
 	# gets logged in user profile, or anonymous profile
 	profile = current_profile(request)
 
@@ -224,7 +241,7 @@ def item_order(request, item_id):
 	return make_order(request, profile, meshu)
 
 # ordering a new meshu
-def order(request):
+def order_new(request):
 	# gets logged in user profile, or anonymous profile
 	profile = current_profile(request)
 
---
+++ static/js/saver.js
@@ -0,0 +1,90 @@
+/*
+  saver.js connects to #save-button and #update-button to save as new and update the current meshu, respectively
+*/
+var saver = function() {
+  var self = {},
+      postSave = null;
+
+  // the object we send to the server to be saved/updated
+  function getMeshuXHR(meshu) {
+    return {
+      'xhr': 'true',
+      'title': meshu.outputTitle(),
+      'svg': meshu.outputSVG(),
+      'location_data' : meshu.outputLocationData()
+    }
+  }
+
+  function updateMeshuID(id) {
+    if (!id) return;
+
+    if ($("#meshu-id").length == 0)
+      $("#hidden-form-values").append('<input type="hidden" id="meshu-id" name="meshu_id" />');
+
+    $("#meshu-id").val(id);
+  }
+
+  self.initialize = function(meshu) {
+    // this only applies to usermade meshus
+    $("#save-button").click(function() {
+      if (!loadedMeshu) return;
+      
+      $("#save-button").html('saving');
+
+      var saveurl = loadedMeshu.edit_url + 'save';
+      $.get(saveurl, getMeshuXHR(meshu), function(data) {
+
+          // create new meshu_id element in the form
+          updateMeshuID(data.meshu_id);
+
+          setTimeout(function() {
+            $("#save-button").html('saved!');
+          }, 200);
+
+          if (postSave)
+            setTimeout(postSave, 500);
+
+          setTimeout(function() {
+            $("#save-button").html('save as new');
+          }, 1000);
+        }, 'json');
+    });
+
+    // update this meshu
+    $("#update-button").click(function() {
+      if (!loadedMeshu) return;
+
+      $("#update-button").html('updating');
+
+      var updateurl = loadedMeshu.edit_url + 'update';
+      $.get(updateurl, getMeshuXHR(meshu), function(data) {
+
+          // create new meshu_id element in the form
+          updateMeshuID(data.meshu_id);
+
+          setTimeout(function() {
+            $("#update-button").html('saved!');
+          }, 200);
+
+          if (postSave) 
+            setTimeout(postSave, 500);
+
+          setTimeout(function() {
+            $("#update-button").html('save current');
+          }, 1000);
+        }, 'json');
+
+    });
+
+    return self;
+  };
+
+  self.saved = function(s) {
+    if (!arguments.length) return postSave;
+
+    postSave = s;
+    return self;
+  };
+
+  return self;
+}();
