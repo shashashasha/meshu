91cb37730124825fd1bb468554c937f22d98ca74
binx <rachelbinx@gmail.com>
2012-03-03 14:14:19 -0800

    banner images and sliding with index.js

---
+++ static/js/index.js
@@ -0,0 +1,10 @@
+$(function() {
+	var index = 0;
+	var sum = $(".bleed").length;
+	setInterval(function(){
+		console.log("fire!");
+		$("#banner-"+index).hide('slide', {direction: 'left'}, 1000);
+		$("#banner-"+(index+1)%sum).show('slide', {direction: 'right'}, 1000);
+		index = (index+1)%sum;
+	},7000);
+});
