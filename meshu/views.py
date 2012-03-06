e54434518560bef8b3bf73dcb9b5f06991c9129c
Sha Hwang <shashashasha@gmail.com>
2012-03-05 22:53:06 -0800

    not letting you shop for anyones meshu :)

--- meshu/views.py
+++ meshu/views.py
@@ -100,6 +100,12 @@ def item_display(request, item_encoded):
 	return item_handler(request, item_id, 'display.html', 'view')
 
 def item_readymade(request, item_id):
+	item = get_object_or_404(Meshu, pk=item_id)
+
+	# don't let people 'shop' for other users items yet
+	if item.user_profile.user.username != 'shop':
+		return notify(request, 'authorization_required')
+
 	return item_handler(request, item_id, 'readymade.html', 'readymade')
 
 def item_delete(request, item_id):
