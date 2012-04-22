42cbff1d2fc40887899405ac2427b508cead4f93
Sha Hwang <shashashasha@gmail.com>
2012-04-22 16:39:04 -0700

    fixing images for readymade, adding product preview for readymades

--- static/js/product.js
+++ static/js/product.js
@@ -6,29 +6,33 @@ $(function() {
 
 		var products = ["earrings","pendant","necklace","cufflinks"];
 		var transforms = ["scale(.09) translate(830, 710) ","scale(.045) translate(1570, 2200)",
-						  "scale(.09) translate(650, 980) ","scale(.075) translate(870, 1170) "];
+						  "scale(.09) translate(650, 980) ","scale(.075) translate(870, 1170)"];
 
 		self.initialize = function(delaunayFrame) {
-
+			// initialize all products
 			for (var i = 0; i < products.length; i++) {
-				var svg = d3.select("#preview-"+products[i]);
-				svg.selectAll("*").remove();
-				
-				svg.append("svg:image")
-					.attr('x', 0)
-					.attr('y', 0)
-					.attr('width', '100%')
-					.attr('height', '100%')
-					.attr('xlink:href', static_url + 'images/preview/preview_' + products[i] + '.png');
-
-				var miniDelaunay = $(delaunayFrame).clone()
-										// .attr("id","")
-										.attr("class","product-delaunay")
-										.attr("transform",transforms[i]);
-
-				$(svg[0]).append(miniDelaunay);
+				self.initializeProduct(products[i], transforms[i], delaunayFrame);
 			}
-		}
+		};
+
+		self.initializeProduct = function(product, transform, delaunayFrame) {
+			var svg = d3.select("#preview-" + product);
+			svg.selectAll("*").remove();
+			
+			svg.append("svg:image")
+				.attr('x', 0)
+				.attr('y', 0)
+				.attr('width', '100%')
+				.attr('height', '100%')
+				.attr('xlink:href', static_url + 'images/preview/preview_' + product + '.png');
+
+			var miniDelaunay = $(delaunayFrame).clone()
+									// .attr("id","")
+									.attr("class","product-delaunay")
+									.attr("transform", transform);
+
+			$(svg[0]).append(miniDelaunay);
+		};
 
 		self.rotation = function(r) {
 			d3.selectAll(".product-delaunay")
--- static/js/readymade.js
+++ static/js/readymade.js
@@ -2,20 +2,51 @@ $(function() {
 	/*
 		How we flip between images of readymades
 	*/
-	var timer;
-	$("#img-thumbs img").click(function(){
-		clearTimeout(timer);
-
-		// <3
+	var previous = null;
+	var thumbnailHandler = function(){
 		var id = $(this).attr("id");
+
 		if (!id) {
-			$(".other-view").removeClass("active");
-			timer = setTimeout(function(){ 
-				$(".other-view").removeClass("z-1"); 
-			}, 1000);
+			return;
 		}
-		else {
-			$("#l-"+id).addClass("active z-1");
+
+		// handle the image thumbnails
+		if (previous) {
+			previous.removeClass('z-2');
+			previous.addClass('z-1');
 		}
-	});
+
+		var next = $("#l-" + id);
+		if (next.length) {
+
+
+			next.removeClass('z-1');
+			next.addClass('z-2');
+			next.fadeIn("normal", function() {
+				if (previous && previous[0] != next[0]) {
+					previous.hide();
+				}
+
+				previous = next;
+			});	
+		} else if (previous) {
+			previous.fadeOut("normal");
+		}
+	};
+	$(".other-view").hide();
+	$("#img-thumbs img").click(thumbnailHandler);
+	$(".readymade-preview").click(thumbnailHandler);
+
+	/*
+		Adding the product preview
+	*/
+	var transforms = {	
+		'earrings': 'scale(.05625) translate(650, 540)',
+		'pendant': 'scale(.03375) translate(1030, 1470)',
+	  	'necklace': 'scale(.05625) translate(510, 760)',
+	  	'cufflinks': 'scale(.05625) translate(510, 760)'
+	};
+	var product = loadedMeshu.product, transform = transforms[product];
+
+	sb.product.initializeProduct(product, transform, $(".delaunay"));
 });
