1e42515c4c3926dfe7f85140d2cafe5c65f2aa55
Sha Hwang <shashashasha@gmail.com>
2012-04-22 17:52:07 -0700

    if no svg, load first image

--- static/js/no-svg.js
+++ static/js/no-svg.js
@@ -1,11 +1,23 @@
 $(function() {
-	$("#modal-bg").fadeIn();
-	$("#edit-help").hide();
-	var div = $("<div>").attr("id","no-svg").append(
-		$("<h2>").text("Oh no, your browser doesn't support SVG. :("),
-		$("<p>").text("Unfortunately, we can't display this page properly in your browser."),
-		$("<p>").html('Why not try with another one, such as <a href="https://www.google.com/chrome" target="_blank">Chrome</a>, <a href="http://www.apple.com/safari/download/" target="_blank">Safari</a>, <a href="http://www.mozilla.org/en-US/firefox/new/" target="_blank">Firefox</a>, or <a href="http://www.microsoft.com/download/en/details.aspx?id=13950">IE9+</a>?'),
-		$("<h2>").html('Or, just check out <a href="/shop/" class="button pink">our shop!</a>')
-	);
-	$("#content").append(div).fadeIn();
+	/*
+		Only block page interaction if we're on the /make page
+	*/
+	if (pageType == 'edit') {
+		$("#modal-bg").fadeIn();
+		$("#edit-help").hide();
+		var div = $("<div>").attr("id","no-svg").append(
+			$("<h2>").text("Oh no, your browser doesn't support SVG. :("),
+			$("<p>").text("Unfortunately, we can't display this page properly in your browser."),
+			$("<p>").html('Why not try with another one, such as <a href="https://www.google.com/chrome" target="_blank">Chrome</a>, <a href="http://www.apple.com/safari/download/" target="_blank">Safari</a>, <a href="http://www.mozilla.org/en-US/firefox/new/" target="_blank">Firefox</a>, or <a href="http://www.microsoft.com/download/en/details.aspx?id=13950">IE9+</a>?'),
+			$("<h2>").html('Or, just check out <a href="/shop/" class="button pink">our shop!</a>')
+		);
+		$("#content").append(div).fadeIn();	
+	} else if (pageType == 'readymade') {
+		// if we don't have svg, by default show the first image
+		// click handler in readymade.js
+		var images = $("#img-thumbs img");
+		if (images.length) {
+			$(images[0]).click();
+		}
+	}
 });
