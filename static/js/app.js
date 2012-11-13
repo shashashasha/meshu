83d3bcc70d60b0f2ee3d32c01be1f19a3f42a2f2
Sha Hwang <shashashasha@gmail.com>
2012-11-12 22:47:02 -0800

    pulling a lot of code dealing with the ordering forms and review order view out of app.js into ui.orderer.js

--- static/js/app.js
+++ static/js/app.js
@@ -16,6 +16,12 @@ $(function() {
 
 	meshu.isReadymade = loadedMeshu && loadedMeshu.product != '';
 
+	// initialize ordering ui
+	sb.ui.orderer(meshu).on("validated", function() {
+		content.attr("class", "review");
+		makeNextView('review');
+	});
+
 	if (loadedMeshu) {
 		// create a saver object, in saver.js
 		saver.initialize(meshu);
@@ -255,6 +261,7 @@ $(function() {
 			case 'edit':
 				// if we were editing and not logged in, show the modal, and save the meshu
 				meshu.updateBounds();
+
 				meshu.mesh().updateCircleBehavior();
 
 				// initialize product picker
@@ -399,205 +406,4 @@ $(function() {
 	};
 
 
-	$("#shipping-destination li").click(function() {		
-        var mode = $(this).attr("id").split("-").pop();
-
-        var form = $("#shipping-destination");
-
-        $("#checkout").attr("class", mode); 
-        form.find("li").removeClass("active");
-        $(this).addClass("active");
-
-        orderer.shippingMode(mode);
-	});
-	/*
-		We're using JQuery validate to check all the forms 
-		of our shipping and credit card input
-
-		If it's valid, we populate the review
-	*/
-	$("#payment-form").validate({
-		rules: {
-		    card_number: {
-		    	creditcard: true
-		    },
-		    card_cvc: {
-		    	digits: true,
-		    	minlength: 3
-		    },
-		    card_month: {
-		    	digits: true,
-		    	minlength: 2
-		    },
-		    card_year: {
-		    	digits: true,
-		    	minlength: 4,
-		    }
-		},
-		messages: {
-			shipping_state: "Please enter the two-letter state abbreviation.",
-			card_cvc: "Sorry, that is not a valid CVC code",
-			card_month: {
-				minlength: "Please enter the month as a two-digit number."
-			}, card_year: {
-				minlength: "Please enter the year as a four-digit number.",
-			},
-		},
-		submitHandler: onFormValidated
-	});
-
-	$("#postcard-note-form").keyup(function(e) {
-		var note = e.target.value;
-		$("#postcard-note").val(note);
-	});
-	
-	$("#coupon-code").submit(function() {
-		var value = $("#coupon-code-value").val();
-		orderer.applyCoupon(value, function(data) {
-			if (data.success) {
-				var couponPrice = parseFloat(data.amount);
-
-				// detect whether the coupon is for an amount or a percentage
-				if (couponPrice < 1 && couponPrice > 0) {
-					var amountOff = Math.floor(orderer.getPrice() * (1 - couponPrice));
-					var percentOff = parseInt((1 - couponPrice) * 100);
-					$("<h2>").attr("id","subtotal-coupon")
-						.addClass("review-header")
-						.html("Coupon:<span>" + percentOff + "% off! -$" + amountOff + ".00</span>")
-						.insertAfter("#subtotal-price");
-				} else if (couponPrice > 1) {
-					$("<h2>").attr("id","subtotal-coupon")
-						.addClass("review-header")
-						.html("Coupon:<span>-$" + couponPrice + ".00</span>")
-						.insertAfter("#subtotal-price");
-				}
-
-				// turn the input form into text
-				$("#coupon-message").fadeIn('fast').html(value + ' discount applied!')
-				$(".coupon-form").hide();
-			} else {
-				$("#coupon-message").fadeIn('fast').html('Invalid code.');
-			}
-
-			populateReview();
-		});
-
-		return false;
-	});
-
-	$("#review-button").click(function() {
-		if (!user.loggedIn) {
-        	forceUserLogin();
-        }
-	});
-
-    $("#submit-button").click(function(e) { 
-        if (!user.loggedIn) {
-        	forceUserLogin();
-        	return false;
-        }
-
-        orderer.submit(); 
-    });
-
-	function onFormValidated() {
-		if (!user.loggedIn) {
-			forceUserLogin(onFormValidated);
-			return;
-		}
-
-		content.attr("class", "review");
-		makeNextView('review');
-	}
-
-	function forceUserLogin(callback) {
-		user.showModal();
-
-		// after a user logs in, click the button
-		user.afterLogIn = function() {
-			if (callback) {
-				saver.createOrUpdateMeshu(function() {
-					// wait a bit... 
-					setTimeout(callback, 200);
-				});	
-			}
-		};
-	}
-
-	/*
-		populateReview runs when we click 'review your order'
-		here's where we populate our hidden form with all of the data we'll be sending
-		also telling stripe how much to charge once the "submit" button is pressed
-	*/
-	function populateReview() {
-		// let our stripe object know what object we're purchasing
-		// it'll know the price, given the options beforehand
-		// we also can't change options once it's set, so no one can mess with it
-		orderer.updateProduct(sb.materializer.product(), sb.materializer.material(), orderer.getShipping());
-
-		$("#object-type").val(sb.materializer.productName());
-		$("#object-material").val(sb.materializer.material());
-		$("#object-color").val(sb.materializer.color().toLowerCase());
-		$("#object-amount").val(orderer.getTotalCents());
-		
-		$("#svg-theta").val(sb.rotator ? sb.rotator.rotation() : 0);
-
-		// outputting meshu data
-		$("#svg-file").val(meshu.outputSVG());
-		$("#meshu-data").val(meshu.outputLocationData());
-		$("#meshu-title").val(loadedMeshu ? loadedMeshu.title : meshu.outputTitle());
-
-		$("#postcard-note").val($("#postcard-note-form").val());
-
-		// update the review
-		updateReviewText();
-
-		$("#review-shipping").empty();
-		$(".ship-row input").each(function(){
-			var div;
-
-			// because i'm anal, sorry
-			// - s.
-			var titleCase = function(str) {
-			    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-			};
-
-			this.value = titleCase(this.value);
-
-			switch (this.id) {
-				case 'ship-city':
-				case 'ship-zip':
-					div = $("<span>");
-					break;
-
-				case 'ship-state':
-					this.value = this.value.toUpperCase();
-					div = $("<span>");
-					break;
-				default:
-					div = $("<p>");
-					break;
-			}
-
-			div.text($(this).val()).appendTo("#review-shipping");
-		});
-		var digits = $("#card-number").val();
-		$("#review-payment").text("XXXX-XXXX-XXXX-"+digits.substring(12,16));
-	}
-	
-	/*
-		Updating the review order divs
-	*/
-	function updateReviewText() {
-
-		var product = "One " + sb.materializer.displayName();
-		var productType = sb.materializer.color().toLowerCase() + " " + sb.materializer.material();
-		$("#review-description").text(product + ", made out of " + productType);
-
-		$("#subtotal-price span").text(orderer.getPriceString());
-
-		$("#shipping-price span").text("$" + orderer.getShipping() + ".00");
-
-		$("#total-price span").text(orderer.getTotalString());
-	}
 });
---
+++ static/js/ui.orderer.js
@@ -0,0 +1,231 @@
+var sb = sb || {};
+
+// using a new sb.ui object, hopefully to pull more out of app.js
+sb.ui = sb.ui || {};
+
+/*
+	
+	Pulling apart some JQuery listeners 
+	from app.js into here. 
+
+	The idea is that stuff in here either 
+	listens to events related to the purchasing 
+	form or modifies the purchasing form.
+
+*/
+sb.ui.orderer = function(meshu) {
+	var self = d3.dispatch("validated", "updated");
+
+	self.on("updated", function() {
+		// update hidden form
+		updateForm();
+
+		// update the 'review order' screen
+		updateReviewText();
+	});
+
+	// switching from international to domestic, or vice versa
+	$("#shipping-destination li").click(function() {		
+        var mode = $(this).attr("id").split("-").pop();
+
+        var form = $("#shipping-destination");
+
+        $("#checkout").attr("class", mode); 
+        form.find("li").removeClass("active");
+        $(this).addClass("active");
+
+        orderer.shippingMode(mode);
+	});
+	
+
+	$("#postcard-note-form").keyup(function(e) {
+		var note = e.target.value;
+		$("#postcard-note").val(note);
+	});
+	
+	$("#coupon-code").submit(function() {
+		var value = $("#coupon-code-value").val();
+		orderer.applyCoupon(value, function(data) {
+			if (data.success) {
+				var couponPrice = parseFloat(data.amount);
+
+				// detect whether the coupon is for an amount or a percentage
+				if (couponPrice < 1 && couponPrice > 0) {
+					var amountOff = Math.floor(orderer.getPrice() * (1 - couponPrice));
+					var percentOff = parseInt((1 - couponPrice) * 100);
+					$("<h2>").attr("id","subtotal-coupon")
+						.addClass("review-header")
+						.html("Coupon:<span>" + percentOff + "% off! -$" + amountOff + ".00</span>")
+						.insertAfter("#subtotal-price");
+				} else if (couponPrice > 1) {
+					$("<h2>").attr("id","subtotal-coupon")
+						.addClass("review-header")
+						.html("Coupon:<span>-$" + couponPrice + ".00</span>")
+						.insertAfter("#subtotal-price");
+				}
+
+				// turn the input form into text
+				$("#coupon-message").fadeIn('fast').html(value + ' discount applied!')
+				$(".coupon-form").hide();
+			} else {
+				$("#coupon-message").fadeIn('fast').html('Invalid code.');
+			}
+
+			self.updated();
+		});
+
+		return false;
+	});
+
+
+	/*
+		We're using JQuery validate to check all the forms 
+		of our shipping and credit card input
+
+		If it's valid, we populate the review
+	*/
+	$("#payment-form").validate({
+		rules: {
+		    card_number: {
+		    	creditcard: true
+		    },
+		    card_cvc: {
+		    	digits: true,
+		    	minlength: 3
+		    },
+		    card_month: {
+		    	digits: true,
+		    	minlength: 2
+		    },
+		    card_year: {
+		    	digits: true,
+		    	minlength: 4,
+		    }
+		},
+		messages: {
+			shipping_state: "Please enter the two-letter state abbreviation.",
+			card_cvc: "Sorry, that is not a valid CVC code",
+			card_month: {
+				minlength: "Please enter the month as a two-digit number."
+			}, card_year: {
+				minlength: "Please enter the year as a four-digit number.",
+			},
+		},
+		submitHandler: onFormValidated
+	});
+
+	$("#review-button").click(function() {
+		if (!user.loggedIn) {
+        	forceUserLogin();
+        }
+	});
+
+    $("#submit-button").click(function(e) { 
+        if (!user.loggedIn) {
+        	forceUserLogin();
+        	return false;
+        }
+
+        orderer.submit(); 
+    });
+
+	function onFormValidated() {
+		if (!user.loggedIn) {
+			forceUserLogin(onFormValidated);
+			return;
+		}
+
+		self.validated();
+	}
+
+	function forceUserLogin(callback) {
+		user.showModal();
+
+		// after a user logs in, click the button
+		user.afterLogIn = function() {
+			if (callback) {
+				saver.createOrUpdateMeshu(function() {
+					// wait a bit... 
+					setTimeout(callback, 200);
+				});	
+			}
+		};
+	}
+
+
+	/*
+		updateForm runs when we click 'review your order'
+		here's where we populate our hidden form with all of the data we'll be sending
+		also telling stripe how much to charge once the "submit" button is pressed
+	*/
+	function updateForm() {
+		// let our stripe object know what object we're purchasing
+		// it'll know the price, given the options beforehand
+		// we also can't change options once it's set, so no one can mess with it
+		orderer.updateProduct(sb.materializer.product(), sb.materializer.material(), orderer.getShipping());
+
+		$("#object-type").val(sb.materializer.productName());
+		$("#object-material").val(sb.materializer.material());
+		$("#object-color").val(sb.materializer.color().toLowerCase());
+		$("#object-amount").val(orderer.getTotalCents());
+		
+		$("#svg-theta").val(sb.rotator ? sb.rotator.rotation() : 0);
+
+		// outputting meshu data
+		$("#svg-file").val(meshu.outputSVG());
+		$("#meshu-data").val(meshu.outputLocationData());
+		$("#meshu-title").val(loadedMeshu ? loadedMeshu.title : meshu.outputTitle());
+
+		$("#postcard-note").val($("#postcard-note-form").val());
+	}
+	
+	/*
+		Updating the review order divs
+	*/
+	function updateReviewText() {
+
+		var product = "One " + sb.materializer.displayName();
+		var productType = sb.materializer.color().toLowerCase() + " " + sb.materializer.material();
+		$("#review-description").text(product + ", made out of " + productType);
+
+		$("#subtotal-price span").text(orderer.getPriceString());
+
+		$("#shipping-price span").text("$" + orderer.getShipping() + ".00");
+
+		$("#total-price span").text(orderer.getTotalString());
+
+		$("#review-shipping").empty();
+		$(".ship-row input").each(function(){
+			var div;
+
+			// because i'm anal, sorry
+			// - s.
+			var titleCase = function(str) {
+			    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+			};
+
+			this.value = titleCase(this.value);
+
+			switch (this.id) {
+				case 'ship-city':
+				case 'ship-zip':
+					div = $("<span>");
+					break;
+
+				case 'ship-state':
+					this.value = this.value.toUpperCase();
+					div = $("<span>");
+					break;
+				default:
+					div = $("<p>");
+					break;
+			}
+
+			div.text($(this).val()).appendTo("#review-shipping");
+		});
+		var digits = $("#card-number").val();
+		$("#review-payment").text("XXXX-XXXX-XXXX-"+digits.substring(12,16));
+	}
+
+	return self;
+};
