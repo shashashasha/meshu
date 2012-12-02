f43383657de33a309db17bfacd0086217b5f8172
Sha Hwang <shashashasha@gmail.com>
2012-12-01 20:54:22 -0800

    fixing routing https proxy

--- static/js/mesh.radial.js
+++ static/js/mesh.radial.js
@@ -267,7 +267,7 @@ sb.mesh.radial = function (frame, map, width, height) {
 
         // jquery automatically adds a callback param
         var mapquest = $('body').hasClass('ie') || window.location.protocol == 'https:' 
-            ? "http://open.mapquestapi.com/directions/v1/route?routeType=pedestrian&outFormat=json&shapeFormat=raw&generalize=200&from=";
+            ? "http://open.mapquestapi.com/directions/v1/route?routeType=pedestrian&outFormat=json&shapeFormat=raw&generalize=200&from="
             : "https://meshu.io/proxy/router/?from=";
 
         var base = mapquest + '{start}&to={end}';
