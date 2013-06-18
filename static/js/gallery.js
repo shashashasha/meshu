b72b0d3d30fc7f1d372a30071d1c9a0c8991a427
binx <rachelbinx@gmail.com>
2013-06-18 14:58:07 -0600

    deleting old stuff from galleryjs

--- static/js/gallery.js
+++ static/js/gallery.js
@@ -1,18 +1,5 @@
 $(function() {
 
-	// var prices = {"earrings":"$75-$150",
-	// 			  "pendant":"$70-$130",
-	// 			  "necklace":"$80-$150"};
-	// var displayNames = {"earrings":"Pair of Earrings",
-	// 					"pendant":"Small Pendant Necklace",
-	// 					"necklace":"Large Necklace",
-	// 					"cufflinks": "Pair of Cufflinks"};
-
-	// $(".description").each(function(){
-	// 	var type = $(this).text();
-	// 	$(this).text(displayNames[type] + ", " + prices[type]);
-	// });
-
 	// if it's a user page, logout redirects to the home page
 	var view = $("#content").attr("class");
 	if (view == 'user') {
